2b74310cdb63c987e5a9cf839d5a9b0f4c463ca5
Jeongbin Bae <bjb6478@naver.com>
2022-03-19 18:48:55 +0900

    object

--- js_basic/app.js
+++ js_basic/app.js
@@ -1,14 +1,14 @@
-const mon ="mon"
-const tue ="tue"
-const wed ="wed"
-const thu ="thu"
-const fri ="fri"
-const sat ="sat"
+//object 속성을 가진 데이터를 저장할 수 있게 함 
 
+const player = {
+    name : "jeongbin",
+    points : 10 ,
+    fat : true ,
 
-const daysOfWeek = [mon ,tue, wed, thu, fri, sat];
-console.log(daysOfWeek); 
-//push 배열에 요소 추가 
-daysOfWeek.push("sun");
-
-console.log(daysOfWeek); 
+};
+console.log(player);
+console.log(player.name);
+console.log(player["name"]);
+player.fat = false; // 속성 변경 가능 
+player.lastName = "patato"; // 언제든지 속성 추가 가능 
+console.log(player);
