ee7b97f551557c7b2ec16a4b7a3604de85ddd541
Jeongbin Bae <bjb6478@naver.com>
2022-03-22 16:05:10 +0900

    eventlistener

--- js_basic/app.js
+++ js_basic/app.js
@@ -1,8 +1,14 @@
-const title =document.getElementsByTagName("h1");
+
 //querySelector는 element를 CSS 방식으로 검색할 수 있음 
-const title1=document.querySelector(".hello h1");
+const title1=document.querySelector("div.hello:first-child h1");
 //querySelector는 하나만 가져오니까 querySelectorAll 해주면 됨 
 
 
-console.log(title);
-console.log(title1);
+// click event 는 클릭하는것 ,click을 listen
+function handleTitleClick(){
+    title1.style.color="blue";
+}
+
+//유저가 title을 클릭하면 실행
+title1.addEventListener("click",handleTitleClick);
+
