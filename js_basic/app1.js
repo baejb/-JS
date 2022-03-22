57f8f5a72deccfbe94e434567fa178e27b11a937
Jeongbin Bae <bjb6478@naver.com>
2022-03-22 18:31:58 +0900

    ..

--- js_basic/app1.js
+++ js_basic/app1.js
@@ -1,21 +1,14 @@
 
 //querySelector는 element를 CSS 방식으로 검색할 수 있음 
-const title2=document.querySelector("div.hello:first-child h1");
+const h1=document.querySelector("div.hello:first-child h1");
 //querySelector는 하나만 가져오니까 querySelectorAll 해주면 됨 
 
 
 // click event 는 클릭하는것 ,click을 listen
 function handleTitleClick(){
-    const currentColor=title2.style.color;
-    let newColor;
-    if(currentColor==="blue"){
-        newColor="tomato";
-    }
-    else{
-        newColor="blue";
-    }
-    title2.style.color = newColor;
+    h1.className = "active";
+    
 }
 
 
-title2.addEventListener("click",handleTitleClick);
+h1.addEventListener("click",handleTitleClick);
